3d2cf02ab5e62d9510344144fb941a621f271e1f
Shubhendu Vaid <shubhendu.vaid@bt.com>
2023-03-26 12:54:27 +0100

    updated sitemap base url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  siteUrl:
-    process.env.NEXT_PUBLIC_BASE_URL || "https://www.discounttrove.co.uk/",
+  siteUrl: "https://www.discounttrove.co.uk/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
